04c2d61ef343121f2500b4cd693df1f4963fd0f2
Mackenzie Bruns <mackenziebruns503@gmail.com>
2020-05-28 20:21:40 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -30,7 +30,12 @@ function whileLoop(n){
      }
   return ("done")
 }
-
+let i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
 
 function doWhileLoop(num){
   do { console.log("I run once regardless.")}
